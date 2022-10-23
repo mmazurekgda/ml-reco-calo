087920399281cfe3593b87b9e404b16b58c319b4
Michal Mazurek <mmazurekgda@gmail.com>
2022-10-23 21:48:39 +0200

    Use dataloader and model in standalone training

--- run.py
+++ run.py
@@ -1,15 +1,14 @@
 import argparse
-import logging as log
+import logging
 import pyfiglet as pf
 import sys
 import os
 from datetime import datetime
+from importlib import import_module
 
 # local
-from cnn.config import Config as CNNConfig
-from cnn.run import CNN
 from utils import activate_logger
-
+from cnn.config import Config as CNNConfig
 
 def parse_options(args, parsed_start_time: str) -> CNNConfig:
     # create the working directory
@@ -17,28 +16,61 @@ def parse_options(args, parsed_start_time: str) -> CNNConfig:
     if not os.path.exists(directory):
         os.makedirs(directory)
     # activate the logger
-    activate_logger(
+    log = activate_logger(
         directory=directory,
-        parsed_start_time=parsed_start_time,
-        logger_level=args.verbosity
+        logger_level=args.verbosity,
     )
     log.info(f"\n{header}")
     log.info("Start time: " + parsed_start_time)
     log.debug("Parsing options MCRecoCalo")
-    config = CNNConfig()
+    config = CNNConfig(
+        load_config_file=args.config_file,
+    )
     for prop, def_value in CNNConfig.OPTIONS.items():
         overriden_text = ""
         value = def_value
         if prop in args:
             parsed_value = getattr(args, prop)
             if parsed_value:
-                overriden_text = "(OVERRIDEN)"
+                overriden_text = "(MANUALLY OVERRIDEN)"
                 value = parsed_value
-        log.debug(f"{prop}: {value} {overriden_text}")
-        setattr(config, prop, value)
+        if overriden_text or not args.config_file:
+            log.debug(f"-> {prop}: {value} {overriden_text}")
+            setattr(config, prop, value)
     return config
 
+def model_lookup(model_name):
+    log = logging.getLogger('MCRecoCalo')
+    if not model_name:
+        raise TypeError("Model must be provided!")
+    model = None
+    models_location = 'cnn.models'
+    this_model_location = ".".join([models_location, model_name])
+    try:
+        model = getattr(import_module(this_model_location), 'model')
+        log.debug(f"-> Imported '{model_name}' from '{models_location}'.")
+    except AttributeError:
+        log.error("-> No model() definition in your model!")
+    except ModuleNotFoundError:
+        log.error(f"-> Could not find '{model_name}' in '{models_location}'")
+    return model
 
+def dataloader_lookup(dataloader_name):
+    log = logging.getLogger('MCRecoCalo')
+    dataloader = None
+    if not dataloader_name:
+        log.debug("-> No dataloader specified. Using the default TFRecords loader.")
+        dataloader_name = 'tfrecords_loader'
+    loaders_location = 'dataloaders'
+    this_loader_location = ".".join([loaders_location, dataloader_name])
+    try:
+        dataloader = getattr(import_module(this_loader_location), 'dataloader')
+        log.debug(f"-> Imported '{dataloader_name}' from '{loaders_location}'.")
+    except AttributeError:
+        log.error("-> No dataloader() definition in your file!")
+    except ModuleNotFoundError:
+        log.error(f"-> Could not find '{dataloader_name}' in '{loaders_location}'")
+    return dataloader
 
 if __name__ == "__main__":
     start_time = datetime.utcnow()
@@ -48,27 +80,50 @@ if __name__ == "__main__":
         description=header,
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
+
+    # main options for running the program from cmd
+
+    parser.add_argument(
+        '--model_name',
+        help='model name (file name)',
+        required=True,
+    )
+
+    parser.add_argument(
+        '--dataloader_name',
+        help='dataloader name (file name)',
+    )
+
     parser.add_argument(
         '--training',
-        help='training on',
-        action='store_false',
+        help='training mode',
+        action='store_true',
     )
+    # TODO: not yet
+    # parser.add_argument(
+    #     '--inference',
+    #     help='inference on',
+    #     action='store_false',
+    # )
+
     parser.add_argument(
-        '--inference',
-        help='inference on',
-        action='store_false',
+        '--config_file',
+        help='preload the configuration file',
+        type=str,
+        default=None,
     )
     parser.add_argument(
         '--directory',
         help='directory where to keep files',
         default=f"./{parsed_start_time}",
     )
-    parser.add_argument(
-        '--type',
-        help='type of framework',
-        default='CNN',
-        choices=['CNN', 'GNN'],
-    )
+    # TODO: not yet
+    # parser.add_argument(
+    #     '--type',
+    #     help='type of framework',
+    #     default='CNN',
+    #     choices=['CNN', 'GNN'],
+    # )
     parser.add_argument(
         '--verbosity',
         help='level of verbosity',
@@ -76,6 +131,7 @@ if __name__ == "__main__":
         choices=['INFO', 'DEBUG'],
     )
 
+    # optional, provide support for config options
     for name, value in CNNConfig.OPTIONS.items():
         prop_type = type(value)
         if prop_type in [int, str, bool, list, float]:
@@ -83,10 +139,34 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     config = parse_options(args, parsed_start_time)
+    log = logging.getLogger('MCRecoCalo')
 
-    framework = CNN()
-    # if args.training:
-    #     framework.train()
+    log.info("Loading model...")
+    model = model_lookup(args.model_name)
+
+    log.info("Loading dataloader...")
+    dataloader = dataloader_lookup(args.dataloader_name)
+
+    log.info("Moving to the main part.")
+    config._freeze()
+
+    from cnn.run import CNN
+    log.info("Instantiating the framework...")
+    framework = CNN(
+        dataloader=dataloader(),
+        model=model(),
+        config=config,
+    )
+
+    if args.training:
+        framework.train()
+    else:
+        log.error("No main action selected.")
+    end_time = datetime.utcnow()
+    execution_time = (end_time - start_time).total_seconds()
+    log.info("End of the program.")
+    log.info(f"Finished at {end_time.strftime('%Y%m%d_%H%M%S%f')}.")
+    log.info(f"Took {execution_time} s.")
 
 
 
